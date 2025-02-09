dc5a8aab56bacb7d3bb6956de9670d4979fbd0bc
Laura Pierzchanowska <lapierzchanowska@gmail.com>
2025-02-09 14:04:51 +0100

    auth logout

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,14 +1,24 @@
 import { useState } from "react";
 import { auth } from "../config/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signOut } from "firebase/auth";
 
 export const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    console.log(auth?.currentUser?.email);
+
     const signIn = async () => {
         try {
             await createUserWithEmailAndPassword(auth, email, password);
+        } catch (err) {
+            console.error(err);
+        }        
+    };
+
+    const logout = async () => {
+        try {
+            await signOut(auth);
         } catch (err) {
             console.error(err);
         }        
@@ -26,6 +36,7 @@ export const Auth = () => {
                 onChange={(e) => setPassword(e.target.value)}
             />
             <button onClick={signIn}>Sign in</button>
+            <button onClick={logout}>Logout</button>
         </div>
     );
 };
